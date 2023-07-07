959bc53135c2b25e100e417ba863a331ff8595a8
ezaguidark <ezaguidavid@gmail.com>
2023-07-07 16:39:08 -0400

    settings

--- config/settings.py
+++ config/settings.py
@@ -163,6 +163,6 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # Costom setting to email
 RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
 
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' 
